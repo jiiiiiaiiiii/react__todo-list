039b11761443f1ea33d12c204ad20012cbc30c85
aqubi <msp827@naver.com>
2023-12-25 21:52:01 +0900

    onClick={onClick}

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,22 +1,22 @@
 import { IToDo } from './atoms';
 
 function ToDo({ text, category }: IToDo) {
-  const onClick = (newCategory: IToDo['category']) => {	// IToDo의 category 인터페이스를 가져옴
-		console.log(newCategory);
-		
+  const onClick = (e:React.MouseEvent<HTMLButtonElement>) => {	// IToDo의 category 인터페이스를 가져옴
+		const {currentTarget: {name}, } = e;
+		console.log(e.currentTarget.name);
 	};
 
   return (
     <li>
       <span>{text} </span>
       {category !== 'DOING' && (
-        <button onClick={() => onClick('DOING')}>Doing</button>
+        <button name='DOING' onClick={onClick}>Doing</button>
       )}
       {category !== 'TO_DO' && (
-        <button onClick={() => onClick('TO_DO')}>To Do</button>
+        <button name='TO_DO' onClick={onClick}>To Do</button>
       )}
       {category !== 'DONE' && (
-        <button onClick={() => onClick('DONE')}>Done</button>
+        <button name='DONE' onClick={onClick}>Done</button>
       )}
     </li>
   );
