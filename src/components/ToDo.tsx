a2060bbac351967788b85bc8b7b462477ac6551e
aqubi <msp827@naver.com>
2023-12-25 21:49:33 +0900

    onClick={() => onClick(Parameter)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,23 @@
 import { IToDo } from './atoms';
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+  const onClick = (newCategory: IToDo['category']) => {	// IToDo의 category 인터페이스를 가져옴
+		console.log(newCategory);
+		
+	};
+
   return (
     <li>
       <span>{text} </span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== 'DOING' && (
+        <button onClick={() => onClick('DOING')}>Doing</button>
+      )}
+      {category !== 'TO_DO' && (
+        <button onClick={() => onClick('TO_DO')}>To Do</button>
+      )}
+      {category !== 'DONE' && (
+        <button onClick={() => onClick('DONE')}>Done</button>
+      )}
     </li>
   );
 }
