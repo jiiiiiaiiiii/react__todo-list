af9137cd6017dd6eb504bc443e5a3ffebb13669f
aqubi <msp827@naver.com>
2023-12-25 21:30:02 +0900

    Refactoring
    Component 정리

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,49 +1,20 @@
-import { useForm } from 'react-hook-form';
-import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: 'TO_DO'|'DOING'|'DONE';
-}
-
-const toDoState = atom<IToDo[]>({
-  key: 'toDo',
-  default: [],
-})
-
+import { useRecoilValue } from 'recoil';
+import { toDoState } from './atoms';
+import CreateToDo from './CreateToDo';
+import ToDo from './ToDo';
 
 function ToDoList() {
-  // const toDos = useRecoilValue(toDoState);       // 1. useRecoilValue: atom에서 값 불러오기
-  // const setToDos = useSetRecoilState(toDoState);    // 2. useSetRecoilState: atom에서 불러온 값 수정
-  const [toDos, setToDos] = useRecoilState(toDoState); // 3. useRecoilState: 1 + 2
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({toDo}: IForm) => {
-    setToDos((oldToDos) => [{text: toDo, id:Date.now(), category: 'TO_DO'}, ...oldToDos]);
-		setValue('toDo', '');	// input 초기화
-  };
+  const toDos = useRecoilValue(toDoState)
 
-  console.log(toDos);
-  
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register('toDo', {
-            required: 'Please write a To Do!',
-          })}
-          placeholder='Write a to do'
-        />
-        <button>Add</button>
-      </form>
+      <CreateToDo />
       <ul>
-        {toDos.map(toDo => <li key={toDo.id}>{toDo.text}</li>)}
+        {toDos.map((toDo) => 
+        <ToDo key={toDo.id} {...toDo} />)}
+        {/* <ToDo key={toDo.id} text={toDo.text} id={toDo.id} category={toDo.category} /> */}
       </ul>
     </div>
   );
