c60c87e4c43c428d1b46dc42b6260a3d9b509f0f
aqubi <msp827@naver.com>
2023-12-25 23:28:51 +0900

    Selectors-1
    ✨selector: derived state(파생된 상태)
    : state를 가져다가(state는 변경❌) -> atom의 변경된 output⭕을 return)
    
    # atoms.tsx
    const toDoSelector = selector({
      key: 'toDoSelector',
      get: ({get}) => { // get Fn: selector의 내부로 atom을 가지고 올 수 있음
        const toDos = get(toDoState); // toDos에 모든 todo가 할당
        return [
          toDos.filter(toDo => toDo.category === 'TO_DO'),
          toDos.filter(toDo => toDo.category === 'DOING'),
          toDos.filter(toDo => toDo.category === 'DONE'),
        ];
      }
    })

--- src/components/atoms.tsx
+++ src/components/atoms.tsx
@@ -1,4 +1,6 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+//✨selector: derived state(파생된 상태)
+// --> state를 가져다가(state는 변경❌) -> atom의 변경된 output⭕을 return)
 
 export interface IToDo {
   text: string;
@@ -10,3 +12,15 @@ export const toDoState = atom<IToDo[]>({
   key: 'toDo',
   default: [],
 });
+
+export const toDoSelector = selector({
+  key: 'toDoSelector',
+  get: ({get}) => { // get Fn: selector의 내부로 atom을 가지고 올 수 있음
+    const toDos = get(toDoState); // toDos에 모든 todo가 할당
+    return [
+      toDos.filter(toDo => toDo.category === 'TO_DO'),
+      toDos.filter(toDo => toDo.category === 'DOING'),
+      toDos.filter(toDo => toDo.category === 'DONE'),
+    ];
+  }
+})
