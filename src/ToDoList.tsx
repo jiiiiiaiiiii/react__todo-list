eb2d236b8ba296920570edb1ffeceb093999e419
aqubi <msp827@naver.com>
2023-12-25 16:16:32 +0900

    React Hook Form
    >npm i react-hook-form
    
    > const {register, watch} = useForm();
    > <input {...register('name')} />
    
    1. register: input 등록 / element 선택 / 유효성 검사규칙 등
    2. watch: input 값의 변화

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,22 +1,48 @@
 import { log } from 'console';
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-function ToDoList() {
+/* function ToDoList() {
 	const [toDo, setTodo] = useState('');
+	const [toDoError, setToDoError] = useState('');
 	const onChange = (e:React.FormEvent<HTMLInputElement>) => {
 		const {currentTarget: {value},} = e;
+		setToDoError('');
 		setTodo(value);
 	};
 	const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log(toDo);
-		
+		if(toDo.length < 10) {
+			return setToDoError('To do should be longer')
+		} 
+		console.log('submit');
 	};
 
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
 				<input value={toDo} onChange={onChange} placeholder='Write a to do' />
+				<button>Add</button>
+				{toDoError !== '' ? toDoError : null}
+			</form>
+		</div>
+	)
+} */
+
+
+function ToDoList() {
+	const {register, watch} = useForm();	// watch: input값의 변화
+	console.log(watch());
+	
+	return (
+		<div>
+			<form>
+				<input {...register('Email')} placeholder='Email' />
+				<input {...register('FirstName')} placeholder='FirstName' />
+				<input {...register('LastName')} placeholder='LastName' />
+				<input {...register('UserName')} placeholder='UserName' />
+				<input {...register('Password')} placeholder='Password' />
+				<input {...register('PasswordConfirm')} placeholder='PasswordConfirm' />
 				<button>Add</button>
 			</form>
 		</div>
