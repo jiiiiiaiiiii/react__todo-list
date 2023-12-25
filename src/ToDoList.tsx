1f7c7c8d1db7880d941e390f76d28d205b64ddda
aqubi <msp827@naver.com>
2023-12-25 17:54:56 +0900

    Custom Validation
    1. setError: 수동으로 Error 설정
    2. shouldFocus: 'name' 요소로 자동 커서 포커스
    
    setError('name', {message: '~ 등'}, { shouldFocus: true })
    
    3. validate: Custom Validation 설정
    
    validate : (값) => boolean

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,4 +1,3 @@
-import { log } from 'console';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -29,58 +28,101 @@ import { useForm } from 'react-hook-form';
 	)
 } */
 
-
 interface IForm {
-	email: string;
-	FirstName: string;
-	LastName: string;
-	UserName: string;
-	Password: string;
-	PasswordConfirm: string;
+  email: string;
+  FirstName: string;
+  LastName: string;
+  UserName: string;
+  Password: string;
+  PasswordConfirm: string;
+  extraError?: string;
 }
 
 function ToDoList() {
-	const {register, handleSubmit, formState: {errors}} = useForm<IForm>({
-		defaultValues: {
-			email: '@naver.com',
-		}
-	});
-	const onValid = (data: any) => {
-		console.log(data);
-	}
-	console.log(errors); // 각 input의 유효성 error 확인
-	
-	return (
-		<div>
-			<form style={{display: 'flex', flexDirection: 'column'}} onSubmit={handleSubmit(onValid)}>
-				<input {...register('email', {
-					required: 'Email is required', 
-					pattern: {
-						value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-						message: 'Only naver.com emails allowed'}
-					}
-				)} placeholder='Email' />
-				<span>{errors?.email?.message as string}</span>
-				<input {...register('FirstName', {required: 'required'})} placeholder='FirstName' />
-				<span>{errors?.FirstName?.message as string}</span>
-				<input {...register('LastName', {required: 'required'})} placeholder='LastName' />
-				<span>{errors?.LastName?.message as string}</span>
-				<input {...register('UserName', {required: 'required', minLength: 5})} placeholder='UserName' />
-				<span>{errors?.UserName?.message as string}</span>
-				<input {...register('Password', {required: 'required', minLength: 10})} placeholder='Password' />
-				<span>{errors?.Password?.message as string}</span>
-				<input {...register('PasswordConfirm', {
-					required: 'Password is required', 
-					minLength: {
-						value: 5,
-						message: 'Your password is too short'
-					},
-				})} placeholder='PasswordConfirm' />
-				<span>{errors?.PasswordConfirm?.message as string}</span>
-				<button>Add</button>
-			</form>
-		</div>
-	)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: '@naver.com',
+    },
+  });
+  const onValid = (data: IForm) => {
+    // console.log(data);
+    if (data.Password !== data.PasswordConfirm) {
+      setError(
+        'PasswordConfirm',
+        { message: 'Passwords are not the same.' },
+        { shouldFocus: true } // PasswordConfirm로 커서 자동 포커스
+      );
+    }
+    // setError('extraError', {message: 'Server offline.'});	// 특정 항목이 아닌, 전체 form에 해당되는 error
+  };
+  console.log(errors); // 각 input의 유효성 error 확인
+
+  return (
+    <div>
+      <form
+        style={{ display: 'flex', flexDirection: 'column' }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: 'Only naver.com emails allowed',
+            },
+          })}
+          placeholder='Email'
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register('FirstName', {
+            required: 'required',
+            validate: {	//✨Custom Validation
+              noAqubi: (value) =>
+                value.includes('aqubi') ? '"aqubi" is not allowed' : true,
+              noZeze: (value) =>
+                value.includes('zeze') ? '"zeze" is not allowed' : true,
+            },
+          })}
+          placeholder='FirstName'
+        />
+        <span>{errors?.FirstName?.message}</span>
+        <input
+          {...register('LastName', { required: 'required' })}
+          placeholder='LastName'
+        />
+        <span>{errors?.LastName?.message}</span>
+        <input
+          {...register('UserName', { required: 'required', minLength: 5 })}
+          placeholder='UserName'
+        />
+        <span>{errors?.UserName?.message}</span>
+        <input
+          {...register('Password', { required: 'required', minLength: 10 })}
+          placeholder='Password'
+        />
+        <span>{errors?.Password?.message}</span>
+        <input
+          {...register('PasswordConfirm', {
+            required: 'Password is required',
+            minLength: {
+              value: 10,
+              message: 'Your password is too short',
+            },
+          })}
+          placeholder='PasswordConfirm'
+        />
+        <span>{errors?.PasswordConfirm?.message}</span>
+        <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
