e34268eec8cf7d9b27684afaab71180f432a0371
aqubi <msp827@naver.com>
2023-12-25 15:29:37 +0900

    basic form event

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import { log } from 'console';
+import React, { useState } from 'react';
 
 function ToDoList() {
+	const [toDo, setTodo] = useState('');
+	const onChange = (e:React.FormEvent<HTMLInputElement>) => {
+		const {currentTarget: {value},} = e;
+		setTodo(value);
+	};
+	const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(toDo);
+		
+	};
+
 	return (
-		<div>ToDoList</div>
+		<div>
+			<form onSubmit={onSubmit}>
+				<input value={toDo} onChange={onChange} placeholder='Write a to do' />
+				<button>Add</button>
+			</form>
+		</div>
 	)
 }
 
